0c4f2395f31b923917728031c8cd3c0591d577b3
rezabox <reza.asareh81@gmail.com>
2023-07-21 23:02:26 +0330

    create a responsive Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import CustomButton from "./CustomButton";
 
-const Navbar = ()=> {
+const Navbar = () => {
   return (
-    <div>
-      <h1>Reza</h1>
-    </div>
-  )
-}
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto 
+      flex item-center justify-between
+      sm:px-16 px-6 py-4">
+        <Link href="/" className="flex items-center justify-between w-full">
+         <Image src='/logo.svg' alt="Car Hub Logo" 
+          width={118} height={18} className="object-contain"/>
+        </Link>
+        <CustomButton
+          title="Sign In"
+          btnType="button"
+          containerStyles="text-primary-blue
+          rounded-full bg-white min-w-[130px]"
+        />
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
