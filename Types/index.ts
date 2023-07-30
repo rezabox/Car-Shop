bebc4dd83cca74051efc5466aade08c4cf1d310b
rezabox <reza.asareh81@gmail.com>
2023-07-30 23:18:49 +0330

    update Type index.ts

--- Types/index.ts
+++ Types/index.ts
@@ -15,3 +15,17 @@ export interface HomePageProps{
    car:string,
    title:string
 }
+export interface CarProps {
+   city_mpg:number;
+   class:string;
+   combination_mpg:number;
+   cylinders:number;
+   displacement:number;
+   drive:string;
+   fuel_type:string;
+   highway_mpg:number;
+   make:string;
+   model:string;
+   transmission:string;
+   year:number;
+}
