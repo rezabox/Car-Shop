d958ad78d0d47d23c5ad0e9871bfab1b6c4edb62
rezabox <reza.asareh81@gmail.com>
2023-07-31 00:55:03 +0330

    updata CarCard

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -9,10 +9,11 @@ interface CarCardProps {
   car: CarProps;  
 }
 const CarCard = ({ car }: CarCardProps)=> {
-  const { city_mpg, year, make, model, transmission } = car;
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  const [isOpen, setIsOpen] = useState(false);
   const carRent = calculateCarRent(city_mpg, year);
   return(
-     <div className="car-card group">
+     <div className="car-card group cursor-pointer">
         <div className="car-card__content">
            <h2 className="car-card__content-title">
             {make} {model}
@@ -38,6 +39,30 @@ const CarCard = ({ car }: CarCardProps)=> {
                     {transmission === 'a' ? 'Automatic': 'Manual'}
                   </p>
                </div>
+               <div className="car-card__icon">
+                  <Image src="/tire.svg" width={20} height={20} alt="steering wheel"></Image>
+                  <p className='text-[14px]'>
+                       {drive.toUpperCase()}
+                  </p>
+               </div>
+               <div className="car-card__icon">
+                  <Image src="/gas.svg" width={20} height={20} alt="steering wheel"></Image>
+                  <p className='text-[14px]'>
+                    {city_mpg} MPG
+                  </p>
+               </div>
+            </div>
+
+            <div className="car-card__btn-container">
+                  <CustomButton
+                      title="View More"
+                      containerStyles="w-full py-[16px] 
+                      rounded-full bg-primary-blue
+                      shadow-transparent" 
+                      textStyles="text-[14px] text-white leading-[17px] font-bold" 
+                      rightIcon="/right-arrow.svg" 
+                      handleClick={()=> setIsOpen(true)}
+                  />
             </div>
         </div>
      </div>
