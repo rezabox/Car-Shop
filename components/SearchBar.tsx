3646c31c2d1ab7d3b14b255b22142e951df84175
rezabox <reza.asareh81@gmail.com>
2023-08-04 14:58:29 +0330

    Create searchbar__item and searchbar__input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,20 @@
 "use client"
 import React, { useState } from 'react'
 import SearchManufacter from './SearchManufacter'
+import Image from 'next/image'
+const SearchButton = ({otherClasses}:{otherClasses:string}) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+     <Image src="/magnifying-glass.svg" alt="magnify" 
+     width={40}
+     height={40}
+     className="object-contain"
+     />
+  </button>
+)
 
 const SearchBar = ()=> {
   const [manufacturer,setManufacturer] = useState('');
+  const [model,setModel] = useState('');
   const handleSearch = () => {}
   return (
     <form className='searchbar' onSubmit={handleSearch}>
@@ -12,7 +23,14 @@ const SearchBar = ()=> {
             manufacturer={manufacturer}
             setManufacturer={setManufacturer}
           />
+          <SearchButton otherClasses="sm:hidden"/>
         </div>
+         <div className="searchbar__item">
+            <Image src="/model-icon.png" alt='model' width={30} height={30}
+            className="absolute w-[20px] h-[20px] ml-4"/>
+            <input type="text" name="model" 
+            value={model} onChange={(e) => setModel(e.target.value)} placeholder='Tiguan' className='searchbar__input' />
+         </div>
     </form>
   )
 }
