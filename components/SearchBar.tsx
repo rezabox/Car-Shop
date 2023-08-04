4f24d6d253f98baf28106e1c0e96f9a3a7fd9ef8
rezabox <reza.asareh81@gmail.com>
2023-08-04 23:00:36 +0330

    add udateSearchParams on handleSearch function and develope this function

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import SearchManufacter from './SearchManufacter'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 const SearchButton = ({otherClasses}:{otherClasses:string}) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
      <Image src="/magnifying-glass.svg" alt="magnify" 
@@ -15,7 +16,36 @@ const SearchButton = ({otherClasses}:{otherClasses:string}) => (
 const SearchBar = ()=> {
   const [manufacturer,setManufacturer] = useState('');
   const [model,setModel] = useState('');
-  const handleSearch = () => {}
+  const router = useRouter();
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(manufacturer === '' && model === '') {
+       return alert("Please fill in the search bar")
+    }
+
+    updateSearchParams(
+      model.toLowerCase(), manufacturer.toLowerCase()
+      )
+  }
+  const updateSearchParams = (model:string,
+   manufacturer: string)=>{
+    const searchParams = new URLSearchParams(window.
+      location.search);
+      if(model) {
+          searchParams.set('model', model);
+      } else {
+          searchParams.delete('model'); 
+      }
+      
+      if(manufacturer){
+          searchParams.set('manufacturer', manufacturer);
+      } else {
+          searchParams.delete('manufacturer');
+      }
+
+      const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+      router.push(newPathname)
+  } 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
         <div className='searchbar__item'>
@@ -30,7 +60,9 @@ const SearchBar = ()=> {
             className="absolute w-[20px] h-[20px] ml-4"/>
             <input type="text" name="model" 
             value={model} onChange={(e) => setModel(e.target.value)} placeholder='Tiguan' className='searchbar__input' />
+            <SearchButton otherClasses="sm:hidden"/>
          </div>
+            <SearchButton otherClasses="max-sm:hidden"/>
     </form>
   )
 }
