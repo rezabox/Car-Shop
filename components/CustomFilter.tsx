2f28bae87a3b8b959a740c4221daf9e8533b1621
rezabox <reza.asareh81@gmail.com>
2023-08-06 19:09:52 +0330

    Create listbox fuels,years and add styles

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client"
+import  { Fragment, useState } from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { Listbox, Transition } from '@headlessui/react'
+import { CustomFilterProps } from '@/Types'
 
-const CustomFilter = () => {
+const CustomFilter = ({ title,options }:CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
+  const router = useRouter();
   return (
-    <div>
-      Custom filter
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={(e)=> setSelected(e)}
+      >
+          <div className="relative w-fit z-10">
+              <Listbox.Button className="custom-filter__btn">
+                  <span className='block truncate'>{selected.title}</span>
+                  <Image
+                    src="/chevron-up-down.svg"
+                    width={20}
+                    height={20}
+                    className='ml-4 object-contain'
+                    alt='chevron up down'
+                    />
+              </Listbox.Button>
+                    <Transition as={Fragment}
+                       leave="transtion ease-in duration-100"
+                       leaveFrom="opacity-100"
+                       leaveTo="opacity-0">
+                        
+                    </Transition>
+          </div>
+      </Listbox>
     </div>
   )
 }
